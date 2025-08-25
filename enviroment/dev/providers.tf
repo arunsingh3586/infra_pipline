cc82f73c5ef4790817c237e48d79ec473714ee22
arunsingh3586 <arunsingh3586@gmail.com>
2025-08-25 22:05:44 +0530

    new providers

---
+++ enviroment/dev/providers.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5e4fafbb-ef69-4e17-8835-68030d81f758"
+}
